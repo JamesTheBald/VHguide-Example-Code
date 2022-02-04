a932f7eb7bf243bdf924a9a12f4774c546d96a42
James Murphy <jamesthebald@gmail.com>
2022-02-04 09:23:13 -0700

    Added Google Tag Manager via Gatsby plugin (gatsby-config.js)

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,51 @@ module.exports = {
   },
 
   plugins: [
+
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-55WGF6P",
+  
+        // Include GTM in development. Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded. Should be an object or a function that is executed in the browser
+        // Defaults to null
+        // defaultDataLayer: { platform: "gatsby" },
+  
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+  
+        // Name of the event that is triggered on every Gatsby route change. Defaults to gatsby-route-change
+        // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+        enableWebVitalsTracking: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`, // See https://www.gatsbyjs.com/plugins/gatsby-plugin-google-gtag/
+      options: {
+        trackingIds: [
+          "G-SYE6NNWMLJ", // Google Analytics aka GA
+        ],
+        // The following object gets passed directly to the gtag config command.This will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   cookie_expires: 0,
+        // },
+        // The following object is used for configuration specific to this plugin
+        pluginConfig: {
+          head: true, // Put tracking script in the head instead of the body  // was false
+          respectDNT: false,  // ""..using Google Global Site Tag does not necessarily constitute Tracking..."
+          // exclude: ["/preview/**", "/do-not-track/me/too/"],     // Avoids sending pageview hits from custom paths
+        },
+      },
+    },
+
+    `gatsby-plugin-react-helmet`,
+
     {
       resolve: "gatsby-plugin-anchor-links",
       options: {
@@ -56,29 +101,7 @@ module.exports = {
       },
     },
 
-    {
-      resolve: `gatsby-plugin-google-gtag`, // See https://www.gatsbyjs.com/plugins/gatsby-plugin-google-gtag/
-      options: {
-        trackingIds: [
-          "G-SYE6NNWMLJ", // Google Analytics aka GA
-        ],
-        // The following object gets passed directly to the gtag config command.This will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // The following object is used for configuration specific to this plugin
-        pluginConfig: {
-          head: false, // Put tracking script in the head instead of the body
-          respectDNT: false,  // ""..using Google Global Site Tag does not necessarily constitute Tracking..."
-          // exclude: ["/preview/**", "/do-not-track/me/too/"],     // Avoids sending pageview hits from custom paths
-        },
-      },
-    },
-
     `gatsby-plugin-postcss`,
-    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
