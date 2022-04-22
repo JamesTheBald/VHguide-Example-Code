db44acbbbe017c1c0968b8b712c4847da32a582a
James Murphy <jamesthebald@gmail.com>
2022-04-22 10:22:05 -0600

    Footer updated for French

--- src/content/navbarLabels.js
+++ src/content/navbarLabels.js
@@ -6,12 +6,13 @@ export const navbarLabels = {
   pearls: { EN: "Clinical Pearls", FR: "Perles cliniques" },
   about: {
     title: { EN: "About", FR: "À propos" },
-    about: { EN: "About the Guide", FR: "À Propos du Guide" },  // Should the French be in title case 
-    media: { EN: "Media Coverage", FR: "Couverture Médiatique" },
-    credits: { EN: "Credits and Collaborators", FR: "Mentions et Collaborateurs" },
+    about: { EN: "About the Guide", FR: "À propos du guide" },  // Should the French be in title case 
+    media: { EN: "Media Coverage", FR: "Couverture médiatique" },
+    credits: { EN: "Credits and Collaborators", FR: "Mentions et collaborateurs" },
   },
+  terms: { EN: "Terms & Notices", FR: "Modalités et avis" },
+  // Thoughts and survey French text is just from google translate
+  thoughts: { EN: "Thoughts on the site?", FR: "Réflexions sur le site?" },
+  // survey: { EN: "Take our survey!", FR: "Répondez à notre sondage!" },
 };
-
-
-
 
